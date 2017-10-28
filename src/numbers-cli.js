6e413557866fc775f365a5c500cce0f9cbbff57c
Josias Iquabius <iquabius@gmail.com>
2017-10-28 16:38:44 -0300

    Run prettier on numbers-cli.js

--- src/numbers-cli.js
+++ src/numbers-cli.js
@@ -1,25 +1,25 @@
 #!/usr/bin/env node
 
-import fetch from 'node-fetch'
-import yargs from 'yargs'
+import fetch from "node-fetch";
+import yargs from "yargs";
 
-let argv = yargs.usage('Usage: $0 [options] number')
-      .example('$0 -t trivia 69', 'Fetch trivia about the number 69')
-      .option('type', {
-        alias: 't',
-        choices: ['date', 'math', 'trivia', 'year'],
-        default: 'trivia',
-        describe: 'Type of fact'
-      })
-      .help('h')
-      .alias('h', 'help')
-      .argv
+let argv = yargs
+  .usage("Usage: $0 [options] number")
+  .example("$0 -t trivia 69", "Fetch trivia about the number 69")
+  .option("type", {
+    alias: "t",
+    choices: ["date", "math", "trivia", "year"],
+    default: "trivia",
+    describe: "Type of fact"
+  })
+  .help("h")
+  .alias("h", "help").argv;
 
 const fetchFact = (type, number) => {
-  console.log(`Fetching ${type} fact for number ${number}...`)
+  console.log(`Fetching ${type} fact for number ${number}...`);
   fetch(`http://numbersapi.com/${number}/${type}`)
     .then(res => res.text())
-    .then(body => console.log(body))
-}
+    .then(body => console.log(body));
+};
 
-fetchFact(argv.type, argv._[0])
+fetchFact(argv.type, argv._[0]);
