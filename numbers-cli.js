534a51b3773f372460348f1ba4483aa6254d2809
Josias Iquabius <iquabius@gmail.com>
2017-10-28 10:59:22 -0300

    Add fact fetching from http://numbersapi.com

---
+++ numbers-cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fetch = require('node-fetch')
+const yargs = require('yargs')
+
+let argv = yargs.usage('Usage: $0 [options] number')
+      .example('$0 -t trivia 69', 'Fetch trivia about the number 69')
+      .option('type', {
+        alias: 't',
+        choices: ['date', 'math', 'trivia', 'year'],
+        default: 'trivia',
+        describe: 'Type of fact'
+      })
+      .help('h')
+      .alias('h', 'help')
+      .argv
+
+const fetchFact = (type, number) => {
+  console.log(`Fetching ${type} fact for number ${number}...`)
+  fetch(`http://numbersapi.com/${number}/${type}`)
+    .then(res => res.text())
+    .then(body => console.log(body))
+}
+
+fetchFact(argv.type, argv._[0])
